73007e24da61b5da080f72b21b57e63ecf4c8eb0
Alex <rollinglex@gmail.com>
2018-06-23 16:07:26 +0900

    setup main component files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    employees: []
+  };
+  render() {
+    return <div className="App">{/* DashBoard
+      Schedule */}</div>;
+  }
+}
+
+export default App;
