6e5ee881a112969e3cc82fe922cee6447a639cca
Dasol-Jung and Epikem <dltldls95@naver.com>
2018-06-27 01:13:27 +0900

    separate ScheduleRow from ScheduleBoard
    Co-authored-by: Dasol-Jung <xenosophian@gmail.com>
    Co-authored-by: Epikem <dltldls95@naver.com>

--- src/components/ScheduleBoard/ScheduleBoard.js
+++ src/components/ScheduleBoard/ScheduleBoard.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import './ScheduleBoard.css';
-const ScheduleRow = (props) => {
-    const week = props.week;
-    const {name, rank, section, schedule} = props.children;
-    return (
-        <tr>
-            <td>{name}</td>
-            <td>{rank}</td>
-            <td>{section}</td>
-            <td>{schedule[week].mon}</td>
-            <td>{schedule[week].tue}</td>
-            <td>{schedule[week].wed}</td>
-            <td>{schedule[week].thu}</td>
-            <td>{schedule[week].fri}</td>
-            <td>{schedule[week].sat}</td>
-            <td>{schedule[week].sun}</td>
-        </tr>
-    );
+import ScheduleRow from '../ScheduleRow';
 }
 const ScheduleBoard = ({ db, week }) => {
     return (
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export {default as ScheduleBoard} from './ScheduleBoard';
+export {default as ScheduleRow} from './ScheduleRow';
 export {default as Dashboard} from './Dashboard';
